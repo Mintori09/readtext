06cc52583ce38f5add74e685405e657f6795fa9c
Mintori <tvmthtr@gmail.com>
2026-02-10 22:32:55 +0700

    feat: Implement syntax highlighting for code blocks

--- src/features/editor/components/MarkdownRenderer.tsx
+++ src/features/editor/components/MarkdownRenderer.tsx
@@ -1,6 +1,25 @@
 import { useEffect, useState, useRef, memo } from "react";
 import { invoke } from "@tauri-apps/api/core";
 import Prism from "prismjs";
+import "prismjs/components/prism-javascript";
+import "prismjs/components/prism-typescript";
+import "prismjs/components/prism-css";
+import "prismjs/components/prism-json";
+import "prismjs/components/prism-markdown";
+import "prismjs/components/prism-python";
+import "prismjs/components/prism-rust";
+import "prismjs/components/prism-bash";
+import "prismjs/components/prism-tsx";
+import "prismjs/components/prism-jsx";
+import "prismjs/components/prism-yaml";
+import "prismjs/components/prism-toml";
+import "prismjs/components/prism-docker";
+import "prismjs/components/prism-sql";
+import "prismjs/components/prism-go";
+import "prismjs/components/prism-c";
+import "prismjs/components/prism-cpp";
+import "prismjs/components/prism-java";
+import "prismjs/components/prism-csharp";
 import { ImageProvider, useImageContext } from "../../../context/ImageContext";
 import { MarkdownRendererProps } from "../types";
 
