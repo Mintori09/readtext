4388d67a6afd62b404f42cf3f6a535e97d5b3014
Tran Van Minh Tri <tvmthtr@gmail.com>
2026-02-13 00:25:27 +0700

    feat: use --max-width

---
+++ build.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+set -e
+
+notify() {
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+        osascript -e "display notification \"$2\" with title \"$1\""
+    elif [[ "$OSTYPE" == "linux-gnu"* ]]; then
+        notify-send "$1" "$2"
+    fi
+}
+
+trap 'notify "Build Failed" "Check terminal"; exit' ERR
+
+pnpm install
+pnpm run build
+pnpm tauri build
+
+notify "Build Success" "App ready in release bundle"
